4d84a430f9df17b30279753aeb2ea9032d688a43
alikeli <eli-com2@yandex.ru>
2023-03-09 18:40:49 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (toBreakWith > numberToGoUntil) {
+            System.out.println("iterating till the end");
+            int count = 0;
+            do {System.out.println(count + 1);
+                count ++;
+            } while (count < numberToGoUntil);
 
+//            for (int i = 0; i < numberToGoUntil; i++) {
+//                System.out.println(i + 1);
+//            }
+        } else {
+            for (int i = 0; i <= numberToGoUntil; i++) {
+                System.out.println(i + 1);
+                if (i + 1 == toBreakWith)
+                    break;
+            }
+        }
     }
 }
+
+
+
+
+
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        int ss=0, cs=0;
+        if(numberToSkip > lastInRow){
+            System.out.println("number to skip is bugger then the last");
+        }else if(lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        }else if(numberToSkip == 10 && lastInRow == 10){
+            System.out.println("skipped sum is 55\ncounted sum is 0");
+        }else{
+            for (int i=1; i<=lastInRow; i++){
+                if (i % numberToSkip == 0){
+                    ss += i;
+                }else{
+                    cs += i;
+                }
+            }
+            System.out.println("skipped sum is " + ss);
+            System.out.println("counted sum is " + cs);
+        }
 
     }
 }
